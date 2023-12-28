4c9b75e574a32bcb46c24fcee31a710028908b19
Ivan Skripnikov <greattramak@mail.ru>
2023-12-28 21:16:15 +0300

    fix tests

--- app/multi_armed_bandit_test.go
+++ app/multi_armed_bandit_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"testing"
 
-	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/IvanSkripnikov/golang_otus_project/components"
 )
 
@@ -24,115 +23,39 @@ func TestGetRating(t *testing.T) {
 }
 
 func TestGetAllEvents(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Errorf("An error '%s' was not expected when opening a stub database connection", err)
-	}
-
-	defer db.Close()
-
-	var str string
-
-	str = "SELECT (.+) from events WHERE type ="
-
-	mock.ExpectQuery(str).WithArgs("show")
-
-	components.SetDatabase(db)
-
-	_ = components.GetAllEvents("show")
-
-	if err = mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %s", err)
+	countEvents := components.GetAllEvents("show")
+	if countEvents == 0 {
+		t.Error("error check events count")
 	}
 }
 
 func TestGetBannerEvents(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Errorf("An error '%s' was not expected when opening a stub database connection", err)
-	}
-
-	defer db.Close()
-
-	var str string
-
-	str = "SELECT (.+) as cnt from events WHERE banner_id = (.+) AND group_id = (.+) AND slot_id = (.+) AND type = (.+)"
-
-	mock.ExpectQuery(str).WithArgs(1, 1, 1, "show")
-
-	components.SetDatabase(db)
-
-	_ = components.GetBannerEvents(1, 1, 1, "show")
-
-	if err = mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %s", err)
+	countBanners := components.GetBannerEvents(1, 1, 1, "show")
+	if countBanners == 0 {
+		t.Error("error check banner events count")
 	}
 }
 
 func TestGetBannersForSlot(t *testing.T) {
-	db, mock, err := sqlmock.New()
+	banners, err := components.GetBannersForSlot(1)
+
 	if err != nil {
-		t.Errorf("An error '%s' was not expected when opening a stub database connection", err)
+		t.Errorf("error while get banners for slot: %v", err)
 	}
-
-	defer db.Close()
-
-	var str string
-
-	str = "SELECT banner_id from relations_banner_slot WHERE slot_id ="
-
-	mock.ExpectQuery(str).WithArgs(1)
-
-	components.SetDatabase(db)
-
-	_, _ = components.GetBannersForSlot(1)
-
-	if err = mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %s", err)
+	if len(banners) == 0 {
+		t.Error("error check banners count")
 	}
 }
 
 func TestGetBannerRatings(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Errorf("An error '%s' was not expected when opening a stub database connection", err)
+	banners := []int{1, 3}
+	result := components.GetBannerRatings(banners, 1, 1)
+
+	if len(result) != 2 {
+		t.Error("error get banners rating")
 	}
 
-	defer db.Close()
-
-	banners := []int{1, 2}
-
-	var str string
-
-	str = "SELECT (.+) as cnt from events WHERE banner_id = (.+) AND group_id = (.+) AND slot_id = (.+) AND type = (.+)"
-
-	mock.ExpectQuery(str).WithArgs(1, 1, 1, "show")
-
-	str = "SELECT (.+) as cnt from events WHERE banner_id = (.+) AND group_id = (.+) AND slot_id = (.+) AND type = (.+)"
-
-	mock.ExpectQuery(str).WithArgs(1, 1, 1, "click")
-
-	str = "SELECT (.+) from events WHERE type ="
-
-	mock.ExpectQuery(str).WithArgs("show")
-
-	str = "SELECT (.+) as cnt from events WHERE banner_id = (.+) AND group_id = (.+) AND slot_id = (.+) AND type = (.+)"
-
-	mock.ExpectQuery(str).WithArgs(2, 1, 1, "show")
-
-	str = "SELECT (.+) as cnt from events WHERE banner_id = (.+) AND group_id = (.+) AND slot_id = (.+) AND type = (.+)"
-
-	mock.ExpectQuery(str).WithArgs(2, 1, 1, "click")
-
-	str = "SELECT (.+) from events WHERE type ="
-
-	mock.ExpectQuery(str).WithArgs("show")
-
-	components.SetDatabase(db)
-
-	_ = components.GetBannerRatings(banners, 1, 1)
-
-	if err = mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %s", err)
+	if result[0].BannerID != 1 || result[0].Value == 0 || result[1].BannerID != 3 || result[1].Value == 0 {
+		t.Error("unexpected result")
 	}
 }
